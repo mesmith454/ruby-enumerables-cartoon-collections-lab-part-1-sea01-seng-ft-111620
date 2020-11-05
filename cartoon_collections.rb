5f6ef8c09a3a0cae5ee8c1079b6c8296f17a320b
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-05 18:55:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,18 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  array.each do |name|
+    puts "Hello " + name + "!"
+  end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  count = 1 
+  array.each_with_index do |name|
+    puts count + ". " + name
+    count += 1 
+  end
 end
