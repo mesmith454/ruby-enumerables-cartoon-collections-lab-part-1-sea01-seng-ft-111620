edbb7b118a38b2e1a98db97ae3f676d957017401
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-05 18:58:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   count = 1 
   array.each_with_index do |name|
-    puts count + ". " + name
+    puts count + name
     count += 1 
   end
 end
